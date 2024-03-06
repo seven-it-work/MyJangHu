7c3f2a7c75ed3d4a11b71b42f2c1cf3dac43b204
seven <sevenyjl@gmail.com>
2024-03-06 17:11:15 +0800

    sql 更新

--- jh-server/t_scene.sql
+++ jh-server/t_scene.sql
@@ -16,3 +16,7 @@ create table t_scene
 alter table t_scene
     owner to postgres;
 
+create index t_scene_city_id_index
+    on t_scene (city_id);
+
+INSERT INTO public.t_scene (city_id, id, name, description, remark, fixed_people_id_list, people_id_list, create_time, update_time) VALUES ('2b49df45da8245758ab798e2c8f50ecb', '4b7553ce68d34978bf3c55d1f17ab7c3', '游身堂', '', '', '[]', '[]', '2024-03-06 00:00:00.000000', '2024-03-06 00:00:00.000000');
